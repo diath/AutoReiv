7cdc7d1157f7bd37277115d378d76a1daf717b47
Kamil Chojnowski <diath@zodiaclabs.org>
2016-08-26 18:00:04 +0200

    Fix the main loop & reconnecting

--- source/run.py
+++ source/run.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
+import asyncio
+import time
+
 from autoreiv import AutoReiv
 
-bot = AutoReiv()
-bot.load()
+def main():
+	while True:
+		bot = AutoReiv()
+		bot.load()
 
-try:
-	bot.run(bot.config.get('login'), bot.config.get('password'))
-except KeyboardInterrupt:
-	bot.close()
-finally:
-	print('* Bye!')
+		try:
+			bot.run(bot.config.get('login'), bot.config.get('password'))
+		except Exception as e:
+			print('* Crashed with error: {}'.format(e))
+		finally:
+			print('* Disconnected.')
+
+		asyncio.set_event_loop(asyncio.new_event_loop())
+
+		print('* Waiting 10 seconds before reconnecting (press ^C to stop)...')
+		try:
+			time.sleep(10)
+		except KeyboardInterrupt:
+			break
+
+if __name__ == '__main__':
+	main()
